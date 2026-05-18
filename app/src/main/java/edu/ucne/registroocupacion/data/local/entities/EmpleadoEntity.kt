1a69d1d034e0a2d12bd72b97a9e8e33cfeedf50e
JP-Guillen <juan_guillen@ucne.edu.do>
2026-05-18 01:59:06 -0400

    Crea EmpleadoEntity.

---
+++ app/src/main/java/edu/ucne/registroocupacion/data/local/entities/EmpleadoEntity.kt
@@ -0,0 +1,15 @@
+package edu.ucne.registroocupacion.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Empleado")
+data class EmpleadoEntity(
+    @PrimaryKey(autoGenerate = true)
+    val empleadoID : Int = 0,
+    val nombres: String,
+    val sexo: String,
+    val sueldo: Double = 0.0,
+    val fechaIngreso : String
+
+)
